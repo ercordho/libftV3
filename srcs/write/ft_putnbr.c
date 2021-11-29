595292c09b396dd4ed4c9795814c093cb410871f
Erwann Cord'Homme <erwannc@macbook-pro-de-erwann.home>
2021-11-29 16:38:23 +0100

    push update ft_putnbr.c

--- srcs/write/ft_putnbr.c
+++ srcs/write/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: ercordho <ercordho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 02:46:58 by ercordho          #+#    #+#             */
-/*   Updated: 2021/11/29 16:24:21 by ercordho         ###   ########.fr       */
+/*   Updated: 2021/11/29 16:38:11 by ercordho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 size_t	ft_putnbr(long nbr)
 {
-	static size_t	size = 0;
+	static size_t	len = 0;
 
 	if (nbr == -2147483648)
 		return (ft_putstr("-2147483648"));
 	if (nbr < 0)
 	{
-		ft_putchar('-');
-		ft_putnbr(nbr * -1);
+		len += ft_putchar('-');
+		len += ft_putnbr(nbr * -1);
 	}
-	else if (nbr > 9)
+	if (nbr > 9)
 	{
-		ft_putnbr(nbr / 10);
-		ft_putnbr(nbr % 10);
+		len += ft_putnbr(nbr / 10);
+		len += ft_putnbr(nbr % 10);
 	}
 	else
-		size += ft_putchar((char)nbr + '0');
-	return (size);
+		len += ft_putchar((char) nbr + '0');
+	return (len);
 }
